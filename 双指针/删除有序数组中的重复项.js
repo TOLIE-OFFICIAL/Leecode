0081195de72a064085a95ba0fb1eeb2267e357bf
TOLIE-OFFICIAL <90889293+TOLIE-OFFICIAL@users.noreply.github.com>
2022-03-31 15:17:07 +0800

    Add files via upload

---
+++ 双指针/删除有序数组中的重复项.js
@@ -0,0 +1,10 @@
+var removeDuplicates = function (nums) {
+    let slow = 1;
+    for (let fast = 1; fast < nums.length; fast++) {
+        if (nums[fast] != nums[fast - 1]) {
+            nums[slow] = nums[fast];
+            slow++;
+        }
+    }
+    return slow;
+};
---
+++ 双指针/移动零.js
@@ -0,0 +1,23 @@
+var moveZeroes = function (nums) {
+    let fast = 0;
+    let slow = 0;
+    let len = nums.length
+    while (fast < len) {
+        // nums[slow]始终指向0
+        console.log(nums[slow]);
+
+        if (nums[fast] != 0) {
+            //交换fast和slow指向的值
+            // a = [b, b=a][0] b=a先执行，所以交换了值
+
+            //注意不能直接给nums[fast]赋值为0
+            nums[slow] = [nums[fast], nums[fast] = nums[slow]][0]
+            slow++;
+        }
+        fast++;
+    }
+    return len;
+};
+
+// moveZeroes([0, 1, 0, 3, 12]);
+moveZeroes([0, 1]);
