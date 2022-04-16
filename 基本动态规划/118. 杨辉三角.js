f03209ba29c00b9538d8968431bfdd4b78cc218c
TOLIE_OFFICIAL <2377397583@qq.com>
2022-04-16 13:53:01 +0800

    git test 3

--- 基本动态规划/118. 杨辉三角.js
+++ 基本动态规划/118. 杨辉三角.js
@@ -1,5 +1,8 @@
+// 直接计算
 var generate = function (numRows) {
+    // 创建一个空数组，用来存储结果
     let result = new Array();
+    // 根据层数遍历
     for (let i = 0; i < numRows; i++) {
         let row = new Array(i + 1).fill(1);
         for (let j = 1; j < row.length - 1; j++) {
